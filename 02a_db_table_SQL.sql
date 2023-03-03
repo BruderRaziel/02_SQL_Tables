e30a3a46f40a385050798b9086ff397c7146d554
BruderRaziel <nico.aldag@web.de>
2023-03-03 10:49:58 +0000

    Kommentare kommentieren

--- 02a_db_table_SQL.sql
+++ 02a_db_table_SQL.sql
@@ -6,3 +6,10 @@
 
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
+
+/* DB löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+/* DB anlegen falls nicht vorhanden */
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
